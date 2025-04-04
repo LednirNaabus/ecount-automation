215dcf0c74fc92e0c09de15b39d75cd853a1abdf
Val Subaan <valsubaan@yahoo.com>
2025-04-04 16:10:46 +0800

    Loggers can be retrieved by name by avoiding adding handlers multiple times

--- utils/logger.py
+++ utils/logger.py
@@ -2,23 +2,24 @@ import logging
 
 class EcountLogger:
     """
-    Just a basic logger for this program.
+    Just a basic logger for the Ecount automation.
     """
-    def __init__(self, filename: str, mode: str, level=logging.NOTSET):
-        self.logger = logging.getLogger('Ecount Logger')
+    def __init__(self, name: str, filename: str, mode: str, level=logging.NOTSET):
+        self.logger = logging.getLogger(name)
         self.logger.setLevel(level=level)
 
-        console_formatter = logging.Formatter("%(name)-12s: %(levelname)-8s %(message)s", datefmt="%Y-%m-%d")
-        console_handler = logging.StreamHandler()
-        console_handler.setFormatter(console_formatter)
+        if not self.logger.handlers:
+            console_formatter = logging.Formatter("%(name)-12s: %(levelname)-8s %(message)s", datefmt="%Y-%m-%d")
+            console_handler = logging.StreamHandler()
+            console_handler.setFormatter(console_formatter)
 
-        file_handler = logging.FileHandler(filename=filename, mode=mode)
-        file_handler.setLevel(logging.DEBUG)
-        file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %I:%M:%S %p")
-        file_handler.setFormatter(file_formatter)
+            file_handler = logging.FileHandler(filename=filename, mode=mode)
+            file_handler.setLevel(logging.DEBUG)
+            file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %I:%M:%S %p")
+            file_handler.setFormatter(file_formatter)
 
-        self.logger.addHandler(console_handler)
-        self.logger.addHandler(file_handler)
+            self.logger.addHandler(console_handler)
+            self.logger.addHandler(file_handler)
 
     def info(self, message):
         self.logger.info(message)
