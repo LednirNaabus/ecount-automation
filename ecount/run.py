26cc8fbe7bd25ccfb37709c55c06b5e1294006fa
Val Subaan <valsubaan@yahoo.com>
2025-04-04 15:26:30 +0800

    Replace export to Excel and Google Sheets to just export to DataFrame

--- ecount/run.py
+++ ecount/run.py
@@ -1,11 +1,12 @@
 import json
 import time
 import pandas as pd
+from typing import Optional, Any, Dict
 
 from dateutil import parser
 from ecount.api import get_zone, login_ecount, get_item_balance_by_location
 from ecount.google_sheets import export_to_google_sheets, create_ingested_sheet, validate_file, check_spreadsheet
-from utils.exporter import export_to_excel
+from utils.exporter import export_to_df
 from utils.bq_utils import load_data_to_bq
 from config import config
 
@@ -51,7 +52,7 @@ def get_formatted_date(base_date):
     parsed_date = parser.parse(base_date)
     return parsed_date.strftime("%Y%m%d")
 
-def load_warehouse_config():
+def load_warehouse_config() -> Optional[dict]:
     try:
         with open('config/config.json', 'r') as file:
             return json.load(file)
@@ -59,34 +60,34 @@ def load_warehouse_config():
         print(f"Error reading config file: {e}")
         return None
 
-def process_warehouses(zone, session_id, warehouses, formatted_date, filename):
+def process_warehouses(zone: str, session_id: str, warehouses: Dict[str, Any], formatted_date: str) -> list[str] | pd.DataFrame:
     empty_warehouses = []
-    data_written = False
-    with pd.ExcelWriter(filename, engine='openpyxl') as writer:
-        warehouse = warehouses.get("Warehouses", {}).items()
-        
-        for i, (warehouse_code, warehouse_name) in enumerate(warehouse):
-            print(f"\nProcessing {warehouse_name}...")
+    dataframe_list = []
+    warehouse = warehouses.get("Warehouses", {}).items()
+    for i, (warehouse_code, warehouse_name) in enumerate(warehouse):
+        print(f"\nProcessing {warehouse_name}...")
+        if i > 0:
+            print(f"Waiting {config.REQUEST_DELAY} seconds before next request...")
+            time.sleep(config.REQUEST_DELAY)
 
-            if i > 0:
-                print(f"Waiting {config.REQUEST_DELAY} seconds before next request...")
-                time.sleep(config.REQUEST_DELAY)
-            
-            inventory_data = fetch_data(zone, session_id, formatted_date, warehouse_code)
+        inventory_data = fetch_data(zone, session_id, formatted_date, warehouse_code)
 
-            if inventory_data:
-                if has_inventory_data(inventory_data):
-                    export_to_excel(writer, inventory_data, warehouse_name, formatted_date, file_ext=".xlsx")
-                    print(f"Exported data for {warehouse_code}:{warehouse_name}")
-                    data_written = True
-                else:
-                    empty_warehouses.append(warehouse_name)
-                    print(f"No data found for {warehouse_name}")
+        if inventory_data:
+            if has_inventory_data(inventory_data):
+                df = export_to_df(inventory_data, warehouse_name, formatted_date)
+                dataframe_list.append(df)
+                print(f"Exported data for {warehouse_code}:{warehouse_name}")
+            else:
+                empty_warehouses.append(warehouse_name)
+                print(f"No data found for {warehouse_name}")
     
-        if not data_written:
-            print("All warehouses returned empty data.")
-            pd.DataFrame(["Empty"]).to_excel(writer, sheet_name=formatted_date,index=False)
-    return empty_warehouses
+    if dataframe_list:
+        combined_df = pd.concat(dataframe_list, ignore_index=True)
+    else:
+        print("All warehouses returned empty data.")
+        combined_df = pd.DataFrame(["Empty"])
+
+    return empty_warehouses, combined_df
 
 def fetch_data(zone, session_id, formatted_date, warehouse_code):
     return get_item_balance_by_location(
@@ -115,24 +116,15 @@ def run():
     if not warehouses:
         return
     
-    filename = f"inventory_balance_asof_{formatted_date}.xlsx"
-    empty_warehouses = process_warehouses(zone, session_id, warehouses, formatted_date, filename)
+    empty_warehouses, df = process_warehouses(zone, session_id, warehouses, formatted_date)
     total_warehouses = len(warehouses.get("Warehouses", {}))
     if len(empty_warehouses) == total_warehouses:
-        print("API response returns empty data for all warehouses. Skipping export to Google Sheets. Try setting another date in your config file.")
+        print("API response returns empty data for all warehouses.")
         return
-
-    print(f"Transferring files to Google Sheets...")
-    excel_file = validate_file(filename)
-    target_spreadsheet = check_spreadsheet(config.GOOGLE_CLIENT, config.SHEET_NAME)
-    exported = export_to_google_sheets(excel_file, target_spreadsheet)
+    
     report_empty_warehouse(empty_warehouses)
-    create_ingested_sheet(target_spreadsheet, exported)
+    print("\nDataframe:")
+    print(df)
 
-    sheet_id = "1EGwJfHGTApzXRnuEv0eQrUccfmZhunaNdkrI-mKPHV"
-    sheet_name = "Sheet1"
-
-    url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
-    df = pd.read_csv(url)
     print("Loading data into BigQuery...")
     load_data_to_bq(df, config.GCLOUD_PROJECT_ID, config.BQ_DATASET_NAME, config.BQ_TABLE_NAME)
