ceedc0002ca7f5b2513d391f628321a25dd65fda
Val Subaan <valsubaan@yahoo.com>
2025-04-04 15:25:47 +0800

    Add export to data frame function to utils module

--- utils/exporter.py
+++ utils/exporter.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from datetime import datetime
-from typing import Literal
+from typing import Literal, Any, Dict
 
 def export_to_excel(writer, data, warehouse_name, date, file_ext: Literal['.xlsx', '.csv']):
     sheet_name = "".join(c for c in warehouse_name if c.isalnum() or c in (' ', '_', '-'))[:31]
@@ -10,3 +10,8 @@ def export_to_excel(writer, data, warehouse_name, date, file_ext: Literal['.xlsx
         df.to_excel(writer, sheet_name=sheet_name, index=False)
     elif file_ext == '.csv':
         df.to_csv(f"inventory_balance_{warehouse_name}_asof_{date}.csv", index=False)
+
+def export_to_df(data: Dict[str, Any], warehouse_name: str, date: str) -> pd.DataFrame:
+    df = pd.DataFrame(data["Data"]["Result"])
+    df['Date'] = datetime.strptime(date, "%Y%m%d").date()
+    return df
