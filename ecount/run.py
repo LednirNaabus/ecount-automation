765eac7841b417cf067df50481b7039a9a01dbe2
Lednir <valsubaan@yahoo.com>
2025-04-07 14:01:17 +0800

    Fix formatting for docstrings.

--- ecount/run.py
+++ ecount/run.py
@@ -19,10 +19,10 @@ def has_inventory_data(response: dict) -> bool:
     
     Parameters:
         response (dict): A dictionary representing the API response.
-                        Expected to have the structure response["Data"]["Result"].
+                        Expected to have the structure `response["Data"]["Result"]`.
 
     Returns:
-        bool: True if response["Data"]["Result"] contains data, otherwise returns False.
+        bool: True if `response["Data"]["Result"]` contains data, otherwise returns False.
     """
     if response["Data"]["Result"]:
         return True
@@ -31,7 +31,7 @@ def has_inventory_data(response: dict) -> bool:
 
 def login() -> Tuple[Optional[str], Optional[str]]:
     """
-    Logs into the system and retrieves the zone and session_id.
+    Logs into the system and retrieves the `zone` and `session_id`.
     Returns:
         tuple: A tuple containing:
             - zone (str or None): The zone string returned from the zone response.
@@ -62,18 +62,18 @@ def login() -> Tuple[Optional[str], Optional[str]]:
     ecount_logger.info(f"SESSION_ID: {session_id}")
 
     if not session_id:
-        ecount_logger.error("No SESSION_ID found.")
+        ecount_logger.error("No `SESSION_ID` found.")
 
     return zone, session_id
 
 def get_formatted_date(base_date: str) -> str:
     """
-    Parses a date string and returns it in 'YYYY/MM/DD' format.
+    Parses a date string and returns it in `YYYY/MM/DD` format.
     Parameters:
         base_date (str): A date string to parse.
 
     Returns:
-        str: The formatted date string in 'YYYY/MM/DD' format.
+        str: The formatted date string in `YYYY/MM/DD` format.
     """
     parsed_date = parser.parse(base_date)
     return parsed_date.strftime("%Y%m%d")
@@ -97,17 +97,17 @@ def process_warehouses(zone: str, session_id: str, warehouses: Dict[str, Any], f
     Processes the data loaded from the warehouse configuration file and returns a list of empty warehouses and a pandas DataFrame of warehouses that contain data.
 
     For each warehouse, this function does the following:
-    1. Fetches inventory data using the provided credentials (zone, session_id, date).
+    1. Fetches inventory data using the provided credentials (`zone`, `session_id`, `formatted_date`).
     2. If data is found and valid, it exports to a pandas DataFrame.
-    3. If no data is found, it appends the warehouse name to the empty_warehouse list.
+    3. If no data is found, it appends the warehouse name to the `empty_warehouse` list.
     4. Returns a DataFrame of combined data if any warehouses had valid inventory data.
     5. If all warehouses returned empty, returnes a DataFrame with a single entry indicating empty data.
 
     Parameters:
-    zone (str): The zone identifier for the session.
-    session_id (str): The session ID retrieved from the login; used for authentication.
-    warehouses (Dict[str, Any]): A dictionary containing warehouse data.
-    formatted_date (str): The date used for querying or labeling, formatted as 'YYYY/MM/DD'.
+        zone (str): The zone identifier for the session.
+        session_id (str): The session ID retrieved from the login; used for authentication.
+        warehouses (Dict[str, Any]): A dictionary containing warehouse data.
+        formatted_date (str): The date used for querying or labeling, formatted as `YYYY/MM/DD`.
 
     Returns:
         Union[list[str], pd.DataFrame]:
@@ -144,18 +144,18 @@ def process_warehouses(zone: str, session_id: str, warehouses: Dict[str, Any], f
 
 def fetch_data(zone: str, session_id: str, formatted_date: str, warehouse_code: str) -> Union[Dict[str, Any], None]:
     """
-    Calls the 'get_item_balance_by_location()' function from api.py with the provided parameters and returns its result. Refer to documentation of 'get_item_balance_by_location()' for more information.
+    Calls the `get_item_balance_by_location()` function from `api.py` with the provided parameters and returns its result. Refer to documentation of `get_item_balance_by_location()` for more information.
 
     Parameters:
         zone (str): The zone string used for this session.
         session_id (str): The session ID retrieved from the login; used for authentication.
-        formatted_date (str): The date used for querying or labeling, formatted as 'YYYY/MM/DD'.
+        formatted_date (str): The date used for querying or labeling, formatted as `YYYY/MM/DD`.
         warehouse_code (str): The unique warehouse identifier.
 
     Returns:
         Union[Dict[str, Any], None]:
             - A dictionary representing the parsed JSON data from the response if available.
-            - None if no data is available or an error occurs in 'get_item_balance_by_location()'.
+            - None if no data is available or an error occurs in `get_item_balance_by_location()`.
     """
     return get_item_balance_by_location(
         base_date=formatted_date,
