ff95632e50cf1b99518a32df4aae4c9ac5e6ff02
Val Subaan <valsubaan@yahoo.com>
2025-04-08 14:43:26 +0800

    Add streamlit page config

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import streamlit as st
 from ecount.run import run
+
+# Config
+st.set_page_config(
+    page_title="Ecount Automation | GoParts",
+    layout="wide"
+)
 
 if __name__ == "__main__":
     """
