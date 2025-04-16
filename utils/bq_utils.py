baa512d2dce713ae6e72d510b627199448b74ca1
Val Subaan <valsubaan@yahoo.com>
2025-04-16 10:36:05 +0800

    Add docstring to functions in utils/bq_utils.py

--- utils/bq_utils.py
+++ utils/bq_utils.py
@@ -10,6 +10,17 @@ from config import config
 from utils.logger import EcountLogger
 
 def get_client():
+    """
+    Returns a dictionary containing BigQuery client configuration details.
+
+    Retrieves the BigQuery client, credentials, and project ID.
+
+    Returns:
+        dict: A dictionary with the BigQuery configuration details.
+            - 'client': BigQuery client.
+            - 'credentials': BigQuery credentials for authorization.
+            - 'project_id': The GCP project ID associated with the credentials.
+    """
     return {
         'client' : config.BQ_CLIENT,
         'credentials' : config.creds,
@@ -17,6 +28,15 @@ def get_client():
     }
 
 def ensure_dataset(logger: EcountLogger, project_id: str, dataset_name: str, client: bigquery.Client):
+    """
+    Checks whether or not the dataset exists in your project.
+
+    Parameters:
+        logger (EcountLogger): Custom Ecount logger.
+        project_id (str): The project ID associated with the credentials.
+        dataset_name (str): The dataset name in your project.
+        client (bigquery.Client): BigQuery client.
+    """
     dataset_id = f"{project_id}.{dataset_name}"
     try:
         client.get_dataset(dataset_id)
@@ -27,6 +47,17 @@ def ensure_dataset(logger: EcountLogger, project_id: str, dataset_name: str, cli
         logger.info(f"Created dataset '{dataset_id}'")
 
 def ensure_table(logger: EcountLogger, project_id: str, dataset_name: str, table_name: str, client: bigquery.Client, schema=None):
+    """
+    Checks if the table exists. If the table exists, the data is appended to the existing table, otherwise, it creates the table and adds the data.
+
+    Parameters:
+        logger (EcountLogger): Custom Ecount logger.
+        project_id (str): The project ID associated with the credentials.
+        dataset_name (str): The dataset name in your project.
+        table_name (str): The table name in the dataset.
+        client (bigquery.Client): BigQuery client.
+        schema (List[SchemaField], optional): Default is None; The BigQuery table schema.
+    """
     table_id = f"{project_id}.{dataset_name}.{table_name}"
     try:
         client.get_table(table_id)
@@ -36,7 +67,26 @@ def ensure_table(logger: EcountLogger, project_id: str, dataset_name: str, table
         client.create_table(table)
         logger.info(f"Created table '{table_id}'")
 
-def load_data_to_bq(logger: EcountLogger, df: pd.DataFrame , project_id: str, dataset_name: str, table_name: str, write_mode="WRITE_APPEND", schema=None):
+def load_data_to_bq(logger: EcountLogger, df: pd.DataFrame , project_id: str, dataset_name: str, table_name: str, write_mode: str ="WRITE_APPEND", schema=None):
+    """
+    Loads a pandas DataFrame into a specified BigQuery table.
+
+    Ensures that the target dataset and table exist before attempting to upload.
+    Supports custom schemas and write modes (e.g., append or overwrite).
+    Logs the outcome using the provided logger.
+
+    Parameters:
+        logger (EcountLogger): Custom Ecount logger.
+        df (pd.DataFrame): Pandas dataframe that will be loaded to BigQuery table.
+        project_id (str): The project ID associated with the credentials.
+        dataset_name (str): The dataset name in your project.
+        table_name (str): The table name in the dataset.
+        write_mode (str): BigQuery write disposition. Default is `WRITE_APPEND`.
+        schema (List[SchemaField], optional): Default is None; The BigQuery table schema.
+
+    Returns:
+        str: A message indicating whether the data upload was successful or failed.
+    """
     client = get_client()['client']
     ensure_dataset(logger, project_id, dataset_name, client)
     ensure_table(logger, project_id, dataset_name, table_name, client, schema)
